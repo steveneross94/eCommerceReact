616891cbda981b50f5f340ae9388ddc4cd31cfd3
steveneross94 <ross.steven@me.com>
2020-11-30 09:37:58 -0500

    hotfix

--- ser-clothing/src/components/collection-item/collection-item.component.jsx
+++ ser-clothing/src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import './collection-item.styles.scss'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
     return (
-        <div className='collection-item'>
+        <div key={id} className='collection-item'>
             <div
                 className='image'
                 style={{
